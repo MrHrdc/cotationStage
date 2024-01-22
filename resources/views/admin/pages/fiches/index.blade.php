a05fa756e177f3d36a7a3d7b56d494dc72ece5fb
Franck kapuya <franckkapuya13@gmail.com>
2024-01-22 09:30:38 +0100

    gestion d'erreur sur les pages index (fiches, stagiaires)

--- resources/views/admin/pages/fiches/index.blade.php
+++ resources/views/admin/pages/fiches/index.blade.php
@@ -38,34 +38,63 @@
               </tr>
             </thead>
             <tbody>
-              @foreach($fiches as $fiche)
-              <tr>
-                <td>{{ $fiche->stagiaire->nom }} {{$fiche->id}}</td>
-                <td>{{ $fiche->stagiaire->postnom }}</td>
-                <td>{{ $fiche->stagiaire->date_fin }}</td>
-                <td>{{ $fiche->coteTotale  }}</td>
-                <td class="text-right  col-lg-2">
-                  <form action="" method="POST">
-                      <a class="btn btn-primary btn-sm" href="{{ route('fiches.show', $fiche->id) }}">
-                          <i class="fas fa-folder">
-                            Voir
-                          </i>
-                      </a>
-                      {{-- @access('delete', 'User') --}}
+              @if(count($fiches) > 0)
+                @foreach($fiches as $fiche)
+                <tr>
+                  <td>{{ $fiche->stagiaire->nom }} {{$fiche->id}}</td>
+                  <td>{{ $fiche->stagiaire->postnom }}</td>
+                  <td>{{ $fiche->stagiaire->date_fin }}</td>
+                  <td>{{ $fiche->coteTotale  }}</td>
+                  <td class="text-right  col-lg-2">
+                    <form action="" method="POST">
+                        <a class="btn btn-primary btn-sm" href="{{ route('fiches.show', $fiche->id) }}">
+                            <i class="fas fa-folder">
+                              Voir
+                            </i>
+                        </a>
+                        {{-- @access('delete', 'User') --}}
+                            @csrf
+                            @method('DELETE')
+                            <a class="btn btn-danger btn-sm" href="{{ route('fiches.destroy', $fiche->id) }}"
+                                onclick="supprimer(event)" item="Voulez-vous supprimer la fiche {{ $fiche->id }}"
+                                data-toggle="modal" data-target="#supprimer">
+                                <i class="fas fa-trash">
+                                  suprimer
+                                </i>
+                            </a>
+                        {{-- @endaccess --}}
+                    </form>
+                  </td>
+                </tr>
+                @endforeach
+                <div class="modal fade" id="supprimer" tabindex="-1" role="dialog" aria-labelledby="modalDeleteLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="modalDeleteLabel">Confirmation de suppression</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <p id="textDelete"></p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        <form id="deleteForm" method="POST" action="{{ route('fiches.destroy', $fiche->id) }}">
                           @csrf
                           @method('DELETE')
-                          <a class="btn btn-danger btn-sm" href="{{ route('fiches.destroy', $fiche->id) }}"
-                              onclick="supprimer(event)" item="Voulez-vous supprimer la fiche {{ $fiche->id }}"
-                              data-toggle="modal" data-target="#supprimer">
-                              <i class="fas fa-trash">
-                                suprimer
-                              </i>
-                          </a>
-                      {{-- @endaccess --}}
-                  </form>
-              </td>
-              </tr>
-              @endforeach
+                          <button type="submit" class="btn btn-danger">Supprimer</button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              @else
+                <tr>
+                  <td colspan="5" class="text-center">Aucune fiche enregistrée.</td>
+                </tr>
+              @endif
             </tbody>
           </table>
 
@@ -75,28 +104,6 @@
   </div>
 </section>
 
-<div class="modal fade" id="supprimer" tabindex="-1" role="dialog" aria-labelledby="modalDeleteLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalDeleteLabel">Confirmation de suppression</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p id="textDelete"></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-        <form id="deleteForm" method="POST" action="{{ route('fiches.destroy', $fiche->id) }}">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-danger">Supprimer</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+
 @endsection
 
