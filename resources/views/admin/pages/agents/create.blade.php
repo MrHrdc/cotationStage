4089e1d0abedfb89060fb44992a985f96915d839
Mrhrdc <enockmukwayanzo123@gmail.com>
2024-01-23 12:08:25 +0100

    ici il nous reste juste les element saisie chez un agent et chez un nouveau stagiaire sois affiche lors des l'enregistrement

--- resources/views/admin/pages/agents/create.blade.php
+++ resources/views/admin/pages/agents/create.blade.php
@@ -23,43 +23,41 @@
           <form method="POST" action="{{ route('agents.store') }}">
             @csrf
             <div class="row">
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputNom" class="form-label">Nom</label>
                 <input type="text" class="form-control" id="inputNom" name="nom" required>
               </div>
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputPostnom" class="form-label">Postnom</label>
                 <input type="text" class="form-control" id="inputNom" name="posttnom" required>
               </div>
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputMatricule" class="form-label">Matricule</label>
                 <input type="text" class="form-control" id="inputNom" name="matricule" required>
               </div>
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputGrde" class="form-label">Grade</label>
                 <input type="text" class="form-control" id="inputNom" name="grade" required>
               </div>
-              <div class="col=6">
+              <div class="col-6">
                 <label for="inputService" class="form-label">Service</label>
                 <input type="text" class="form-control" id="inputNom" name="nom" required>
               </div>
-              <div class="col=6">
+              <div class="col-6">
                 <label for="inputEmail" class="form-label">Email</label>
                 <input type="email" class="form-control" id="inputEmail" name="email" required>
               </div>
-            </div>
             <div class="row">
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputPrenom" class="form-label">Prénom</label>
                 <input type="text" class="form-control" id="inputPrenom" name="prenom" required>
               </div>
-            </div>
             <div class="row">
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputPassword" class="form-label">Mot de passe</label>
                 <input type="password" class="form-control" id="inputPassword" name="password" required>
               </div>
-              <div class="col=4">
+              <div class="col-6">
                 <label for="inputFonction" class="form-label">Fonction</label>
                 <select class="form-control" id="inputFonction" name="fonction_id" required>
                   @foreach ($fonctions as $fonction)
@@ -67,7 +65,6 @@
                   @endforeach
                 </select>
               </div>
-            </div>
             <div class="text-right mt-2">
               <button type="submit" class="btn btn-primary">Créer</button>
               <button type="reset" class="btn btn-secondary">Réinitialiser</button>
--- resources/views/admin/pages/stagiaires/create.blade.php
+++ resources/views/admin/pages/stagiaires/create.blade.php
@@ -34,45 +34,46 @@
               </select>
             </div>
             <div class="col-md-6">
-              <label for="inputSexe" class="form-label">Sexe</label>
+              <label for="inputSexe" class="form-label">categorie</label>
               <select class="form-control" name="sexe" id="inputSexe">
                 <option value="null">Selectionnez votre categorie</option>
                 <option value="homme">Academique</option>
                 <option value="femme">Professionnelle</option>
               </select>
-            <div class="col-md-12">
+</div>
+            <div class="col-md-6">
               <label for="inputPostnom" class="form-label">Postnom</label>
               <input type="text" class="form-control" name="postnom" id="inputPostnom">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputPrenom" class="form-label">Prénom</label>
               <input type="text" class="form-control" name="prenom" id="inputPrenom">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputLieuAffection" class="form-label">Lieu d'affection</label>
               <input type="text" class="form-control" name="lieu_affection" id="inputLieuAffection">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputDomaineStage" class="form-label">Domaine du stage</label>
               <input type="text" class="form-control" name="domaine_stage" id="inputDomaineStage">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputNumerodivision" class="form-label">Numero division</label>
               <input type="text" class="form-control" name="domaine_stage" id="inputNumerodivision">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputInstitutionProvenance" class="form-label">Institution de provenance</label>
               <input type="text" class="form-control" name="institution_provenance" id="inputInstitutionProvenance">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputAdressedomicilee" class="form-label">adresse Domicile</label>
               <input type="text" class="form-control" name="domaine_stage" id="inputAdressedomicile">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputDateDebut" class="form-label">Date de début</label>
               <input type="date" class="form-control" name="date_debut" id="inputDateDebut">
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
               <label for="inputDateFin" class="form-label">Date de fin</label>
               <input type="date" class="form-control" name="date_fin" id="inputDateFin">
             </div>
